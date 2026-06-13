464272bbbdf4da494d2130e26e33629f7574e569
UjjwalVerma1411 <ujjwal14professional@gmail.com>
2026-06-14 00:35:41 +0530

    Add files via upload

---
+++ Week1/Day1/about_me.py
@@ -0,0 +1,19 @@
+print("===== ABOUT ME =====")
+
+print("Name: Ujjwal Verma")
+print("College: Manipal University Jaipur")
+print("Year: Second Year")
+
+print("\nWhy I Am Learning Python:")
+print("I am learning Python because it is one of the most important programming languages for AI, automation, software development, and freelancing.")
+
+print("\nMy 3-Year Goal:")
+print("Over the next three years, my goal is to become a skilled AI Engineer.")
+print("I will build projects, contribute on GitHub, and strengthen my portfolio.")
+print("I will also excel Python, C, and DSA.")
+
+print("\nCurrent Mission:")
+print("Master Python fundamentals and build a strong programming foundation.")
+
+print("\nFavorite Technology:")
+print("The technologies that interest me the most are Artificial Intelligence (AI) and Quantum Computing.")
---
+++ Week1/Day1/comment_practice.py
@@ -0,0 +1,15 @@
+#Comments are notes written for humans.
+
+#Single line comment - #
+
+#Display the creator's name
+print("Ujjwal Verma")
+
+
+#Multi Line Comment
+"""
+Technically, Python does not have a true multi-line comment syntax.
+Python creates a string object and then immediately discards it because it's not assigned to a variable.
+So, it behaves like a comment, but it isn't one.
+These are actually multi-line strings, not comments.
+"""
